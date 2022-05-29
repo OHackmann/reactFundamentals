b770040393f046cb7e99d31e1424d2772852aa19
Hackmann <hackmann657@gmail.com>
2022-05-29 10:46:58 -0300

    finalização do projeto

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,22 +1,43 @@
 import React, { Component } from "react";
+import './Contador.css'
+import Display from "./Display";
+import PassoForm from "./PassoForm";
+import Botoes from "./Botoes";
 
 export default class Contador extends Component {
 
     state = {
-        passo: 1,
-        valor: 0
+            passo: this.props.passo || 1,
+            valor: this.props.valor || 0
+        }
+
+    inc = () => {
+        this.setState({
+            valor: this.state.valor + this.state.passo
+        })
     }
+
+    dec = () => {
+        this.setState({
+            valor: this.state.valor - this.state.passo
+        })
+    }
+
+    mudarPasso = (novoPasso) => {
+        this.setState({
+            passo: novoPasso
+        })
+    }
+    
 
     render() {
         return (
-            <div>
+            <div className="Contador">
                 <h2>Contador</h2>
-                <h4>Valor: {this.state.props}</h4>
-
-                <div>
-                    <button>+</button>
-                    <button>-</button>
-                </div>
+                    <PassoForm passo ={this.state.passo}
+                        onPassoChange={this.mudarPasso}></PassoForm>
+                    <Display valor={this.state.valor} />
+                    <Botoes onInc={this.inc} onDec={this.dec} />
             </div>
         )
     }
